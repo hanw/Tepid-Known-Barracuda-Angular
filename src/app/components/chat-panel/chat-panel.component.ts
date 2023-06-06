9721475abcc45885e1793eb7d84df64da403e951
Han Wang <han2.wang@intel.com>
2023-06-06 19:40:48 +1200

    Commit from TeleportHQ

--- src/app/components/chat-panel/chat-panel.component.ts
+++ src/app/components/chat-panel/chat-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'chat-panel',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core'
   styleUrls: ['chat-panel.component.css'],
 })
 export class ChatPanel {
+  @Input()
+  image_src: string =
+    'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixid=M3w5MTMyMXwxfDF8YWxsfDF8fHx8fHwyfHwxNjg2MDMzNTAxfA&ixlib=rb-4.0.3&w=500'
+  @Input()
+  image_alt: string = 'image'
+  @Input()
+  image_src1: string =
+    'https://images.unsplash.com/photo-1685738538054-9953b5461dba?ixid=M3w5MTMyMXwwfDF8YWxsfDE0fHx8fHx8Mnx8MTY4NjAzMzUwMXw&ixlib=rb-4.0.3&w=500'
+  @Input()
+  image_alt1: string = 'image'
+  @Input()
+  image_src2: string =
+    'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixid=M3w5MTMyMXwwfDF8YWxsfDE1fHx8fHx8Mnx8MTY4NjAzMzUwMXw&ixlib=rb-4.0.3&w=500'
+  @Input()
+  image_alt2: string = 'image'
   constructor() {}
 }
