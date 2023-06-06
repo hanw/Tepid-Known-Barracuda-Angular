3cf39d1e1fa78277ecc0fec3f3330f388c038359
Han Wang <han2.wang@intel.com>
2023-06-06 15:48:24 +1200

    Commit from TeleportHQ

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,6 +2,8 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
 
+import '@lottiefiles/lottie-player'
+
 import { ComponentsModule } from '../../components/components.module'
 import { Home } from './home.component'
 
